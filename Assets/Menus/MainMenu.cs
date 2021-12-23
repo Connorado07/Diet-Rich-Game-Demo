cb6abd1f6fd481ed67557e5cb7a22655ff73707e
ronnoxtyb <ronnoxtyb@gmail.com>
2021-12-23 14:37:16 -0800

    Added Main Menu and others -CD

---
+++ Assets/Menus/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void NewGame()
+	{
+		Debug.Log("A new game has been created.");
+		SceneManager.LoadScene("Space Station");
+	}
+	public void LoadGame()
+	{
+		Debug.Log("The previous game has been loaded.");
+	}
+	public void OptionsMenu()
+	{
+		Debug.Log("The options menu has been opened.");
+		SceneManager.LoadScene("Options");
+	}
+    public void QuitGame()
+	{
+		Debug.Log("The game has quit.");
+        Application.Quit();
+	}
+}
+
